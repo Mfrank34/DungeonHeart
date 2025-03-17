e21f34e4a58af4d3091349f48053eea03b4e52e9
Mfrank34 <123030830+Mfrank34@users.noreply.github.com>
2025-03-17 23:23:30 +0000

    adding
    fixes to game load and spawing system, adding more tomorrow.

--- Script/main_scene.gd
+++ Script/main_scene.gd
@@ -1,11 +1,18 @@
 extends Control
 # Thanks to this guy for help: https://youtu.be/a0UQ-t-vuzY?si=woHZ2jEeqXjkr1nm
 
+# ready on load
 @onready var hud : Control = $HUD
 @onready var menu : Control = $Menu
 @onready var main_2d : Node2D = $Main2D
 @onready var camera : Camera2D = $Main2D/Camera
+
+# setting for maps
 var level_instance : Node2D
+var maps = ["Level_1", "Level_2", "Level_3"]
+var enemys = ["Enemy_1", "Enemy_2", "Enemy_3"]
+var map_limits_min = Vector2 (25, 15) # Top-left corner
+var map_limits_max = Vector2 (24 , 15) # Bottom-right corner
 
 func _ready() -> void:
 	pass
@@ -18,9 +25,10 @@ func unload_level():
 
 func load_level(level_name : String):
 	unload_level()
+	# gets the path to load and remove the ending so just name to get with string
 	var level_path := "res://Scenes/Room/%s.tscn" % level_name
 	var level_resource := load(level_path)
-	
+	# error checking map loading
 	if level_resource:
 		level_instance = level_resource.instantiate()
 		main_2d.add_child(level_instance)
@@ -46,6 +54,8 @@ func load_enemy(enemy_name: String, min_pos: Vector2, max_pos: Vector2):
 
 func _on_start_pressed() -> void:
 	load_level("Level_1")
+	load_enemy("Enemy_1", map_limits_min, map_limits_max)
+
 
 func _on_exit_pressed() -> void:
 	unload_level()
